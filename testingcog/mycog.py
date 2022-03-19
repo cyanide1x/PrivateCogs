d0e1e565a67335c603bddc6a6a978badece820b3
cyanide <76759085+cyanide1x@users.noreply.github.com>
2022-03-18 22:26:34 -0500

    Update mycog.py

--- testingcog/mycog.py
+++ testingcog/mycog.py
@@ -5,6 +5,8 @@ class MyCog(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        bot.add_dev_env_value("mycog", lambda ctx: self)
+        bot.add_dev_env_value("mycogdata", lambda ctx: self.settings[ctx.guild.id])
 
     @commands.command()
     async def mycom(self, ctx):
