835daf3930962faaa1a09b02baa2d733bb8b293e
cyanide <76759085+cyanide1x@users.noreply.github.com>
2022-03-18 22:29:22 -0500

    Update mycog.py

--- testingcog/mycog.py
+++ testingcog/mycog.py
@@ -1,12 +1,18 @@
 from redbot.core import commands
 
+
 class MyCog(commands.Cog):
     """My custom cog"""
 
     def __init__(self, bot):
         self.bot = bot
         bot.add_dev_env_value("mycog", lambda ctx: self)
-        bot.add_dev_env_value("mycogdata", lambda ctx: self.settings[ctx.guild.id])
+        bot.add_dev_env_value(
+            "mycogdata", lambda ctx: self.settings[ctx.guild.id])
+
+    def cog_unload(self):
+        self.bot.remove_dev_env_value("mycog")
+        self.bot.remove_dev_env_value("mycogdata")
 
     @commands.command()
     async def mycom(self, ctx):
